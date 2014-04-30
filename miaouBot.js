8d5be16f26f04e9f5e306dd04917c55672e80276
SteamWind <sylvain.pillet@gmail.com>
2014-04-30 10:52:01 +0200

    Added some commands
    By default, no modes are activated.
    Added afk, pingback, fancy and !@all commands
    Some other improvments

--- miaouBot.js
+++ miaouBot.js
@@ -85,20 +85,28 @@ var flipTable = {
     "\u2045": "\u2046"
 };
 
+var fancy = false;
+var pingBack = false;
+var afk = false;
+var afkMessage = "";
+
 var code = function () {
 
     if (!miaou || !miaou.chat) return;
+
 
     var IAmPingedRegex = new RegExp('@' + me.name + '(\\b|$)');
     // when a message comes in, let's handle it
     miaou.chat.on('incoming_message', function (m) {
         if (!(m.created > miaou.chat.enterTime)) return; // we only handle new messages
-        if (IAmPingedRegex.test(m.content) && m.author !== me.id) {
+        if (IAmPingedRegex.test(m.content) && m.author !== me.id && pingBack) {
             // we've been pinged, let's pong, maybe
             var delay = 5000 * Math.random();
             if (delay > 2000) setTimeout(function () {
                 miaou.chat.sendMessage("@" + m.authorname + "#" + m.id + " " + (~m.content.indexOf("pong") ? "ping" : "pong!"));
             }, delay);
+        } else if (IAmPingedRegex.test(m.content) && m.author !== me.id && afk) {
+            miaou.chat.sendMessage("@" + m.authorname + "#" + m.id + " " + "AFK: " + afkMessage);
         } else if (/^echo [^\n]+$/.test(m.content)) {
             // somebody wants us to repeat, let's do it
             miaou.chat.sendMessage(m.authorname + " says\n> " + m.content.slice("echo ".length));
@@ -108,14 +116,49 @@ var code = function () {
     var deco = ['', '*', '**', '***', '---', '`', ' '];
     // when a message is sent by the host user, let's make it prettier
     miaou.chat.on('sending_message', function (m) {
+        if (/!@all/.test(m.content)) {
+            var allUserArray = $('#users .user').map(function () {
+                return $(this).data('user').name
+            }).get().filter(function (n) {
+                    return n !== me.name
+            });
+            allUserArray.forEach(function (user, index) {
+                allUserArray[index] = "@" + user;
+            });
+            var allUsers = allUserArray.join(", ");
+            m.content = m.content.replace("!@all", allUsers);
+        }
         if (/^[\-*`]*flip[\-*`]* [^\n`*\/]*$/.test(m.content)) {
             m.content = flipString(m.content.slice("flip ".length));
         } else if (/^[\-*`]*yoda[\-*`]* [^\n`*\/]*$/.test(m.content)) {
             m.content = yoda(m.content.slice("yoda ".length));
-        } else if (/^\w[^\n`*\/]*$/.test(m.content)) {
+        } else if (/^[\-*`]*afk[\-*`]* [^\n`*\/]*$/.test(m.content)) {
+            afkMessage = m.content.slice("afk ".length);
+            afk = true;
+            console.log("AFK: " + afkMessage);
+        } else if (/^[\-*`]*afk[\-*`]*/.test(m.content)) {
+            afkMessage = "";
+            afk = false;
+            console.log("Back from AFK");
+            m.message("I'm back.");
+        } else if (/^[\-*`]*fancy[\-*`]*/.test(m.content)) {
+            fancy = !fancy;
+            var fancyActivated = fancy ? "activated" : "disabled";
+            console.log("Fancy mode " + fancyActivated);
+            alert("Fancy mode " + fancyActivated);
+            return false;
+        } else if (/^[\-*`]*pingback[\-*`]*/.test(m.content)) {
+            pingBack = !pingBack;
+            var pingBackActivated = pingBack ? "activated" : "disabled";
+            console.log("PingBack mode " + pingBackActivated);
+            alert("PingBack mode " + pingBackActivated);
+            return false;
+        } else if (/^\w[^\n`*\/]*$/.test(m.content) && fancy) {
             m.content = m.content.split(' ').map(function (t, b) {
                 return b = deco[~~(Math.random() * deco.length)], b + t + b;
             }).join(' ');
+        } else {
+//            m.content = m.content;
         }
     });
 
@@ -161,3 +204,5 @@ function yoda(aString) {
     console.log(stringSentences);
     return stringSentences;
 }
+
+console.info("miaouBot activated!");
--- miaouBotREADME.md
+++ miaouBotREADME.md
@@ -14,8 +14,30 @@ Exemple:
 ####echo
 Write back what you said
 
+####afk [message]
+If you write afk with a message, it would ping back the person who ping you and deliver the message you setted.
+
+You can set the message anytime. When you come back, just write "afk" and it would stop this.
+
+####pingback _(toggle)_
+Type it once to activate it, it will be disabled if you type it the second time.
+
+This command ping back anyone who ping you but with a delay.
+
+####fancy _(toggle)_
+Type it once to activate it, it will be disabled if you type it the second time.
+
+This command enable a random formatting of your messages.
+
 ####flip
 Put your text upside down
 
 ####yoda
 Modify the order of the words in each sentences and make you wiser (or dumber)
+
+####!@all
+Put it anywhere in your message and it will ping everyone in the room. Be careful, avoid ban by using this with parsimony.
+
+Exemple:
+
+`Hello !@all`
